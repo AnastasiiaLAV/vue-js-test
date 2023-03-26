6e41869a539d6c9452a8cc5b476895658bcfa600
Anastasiia Lytviak <lav21051991@gmail.com>
2023-03-26 21:22:17 +0300

    added custom input

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,8 @@
 <template>
+  <div>
+  <h2>{{text}}</h2>
+  <InputVue v-model="text"/>
+
     <ApartmentsList :items="apartments">
       <template v-slot:apartment="{apartment}">
           <ApartmentItem
@@ -7,28 +11,38 @@
             :rating="apartment.rating"
             :price="apartment.price"
             :imgUrl="apartment.imgUrl"
+            @click="handleItemClick"
         />
       </template>
     </ApartmentsList>
+    </div>
 </template>
 
 <script>
 import ApartmentsList from './components/ApartmentsList/ApartmentsList.vue' ;
 import apartments from './components/ApartmentsList/apartments';
 import ApartmentItem from './components/Apartment/ApartmentItem.vue';
+import InputVue from './components/IndividualComponents/InputVue.vue';
 
 export default {
   name: 'App',
   components: {
       ApartmentsList,
-      ApartmentItem
+      ApartmentItem,
+      InputVue,
       },
     data(){
       return{
+        text: "",
         apartments,
       }
       
-    }
+    },
+    methods: {
+        handleItemClick (){
+          console.log('click')
+        }
+      },
   }
 </script>
 
---
+++ src/components/IndividualComponents/InputVue.vue
@@ -0,0 +1,62 @@
+<template lang="">
+    <div>
+        <input 
+        type="text"  
+        v-model="text"
+        v-on="listeners"
+        v-bind="$attrs"
+        class="custom-input"/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "InputVue",
+    data(){
+        return{
+            text: ""
+        }
+    },
+    computed:{
+        listeners(){
+            return{
+                ...this.$attrs,
+                input: event => this.$emit('input', event.target.value)
+            }
+        }
+    },
+    // methods: {
+    //     InputEvent (){
+    //       console.log(event.target.value)
+    //     }
+    //   },
+}
+</script>
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+    .custom-input {
+        height: 40px;
+        /* width: 100%; */
+        width: 200px;
+        border: 2px solid $main-color;
+        font-size: 18px;
+        outline: none;
+        line-height: inherit;
+        padding: 8px 15px;
+        &::placeholder {
+            color: inherit;
+        }
+        &--error {
+            border-color: red;
+        }
+        &__error {
+            position: absolute;
+            top: 100%;
+            right: 0;
+            width: 100%;
+            font-size: 12px;
+            color: red;
+            line-height: 1.3;
+        }
+    }
+</style>
